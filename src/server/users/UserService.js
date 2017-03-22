27bd905820e8edd2987be1517c71f64c0975ca3a
strothj <jason@jasons.io>
2017-03-22 03:39:00 -0400

    Added UserService find

--- src/server/users/UserService.js
+++ src/server/users/UserService.js
@@ -28,7 +28,7 @@ export const create = async (user, args) => {
   // Only admin users can create users of type "admin" and "editor".
   const isAdmin = Boolean(user && user.role === 'admin');
   if (!isAdmin && (args.user.role === 'admin' || args.user.role === 'editor')) {
-    return Promise.reject(new PrivilegeError('Need admin privilege to creat that user type'));
+    return Promise.reject(new PrivilegeError('Need admin privilege to create that user type'));
   }
 
   return User.create(args.user);
@@ -42,16 +42,20 @@ export const create = async (user, args) => {
  * @param {string} args.id The ID of the user to return.
  * @returns {Promise<Object>} The requested user.
  */
-export const findOne = async (user, args) => User.findOne({ _id: args.id });
+export const findOne = (user, args) => User.findOne({ _id: args.id });
 
 /**
  * Returns a list of users.
  *
  * @param {object} user Authenticated user or null if no user is authenticated.
  * @param {object} args Arguments passed in by Swagger middleware.
- * @param {args.page} [number] Which page of results to return.
- * @param {args.per_page} [number] How many results per page to return.
+ * @param {args.page} number Which page of results to return.
+ * @param {args.per_page} number How many results per page to return.
  */
 export const find = async (user, args) => {
+  if (user.role !== 'admin') return Promise.reject(new PrivilegeError('Need admin privilege to list users'));
 
+  const totalUsers = await User.count();
+  const users = await User.find().skip(args.per_page * args.page).limit(args.per_page);
+  return Promise.resolve({ totalUsers, users });
 };
--- src/server/users/UserService.spec.js
+++ src/server/users/UserService.spec.js
@@ -13,14 +13,14 @@ describe('User service', () => {
   beforeEach(() => User.remove());
 
   describe('create', () => {
-    const createUser = (authenticatedUser, newUserParam) => (
+    const create = (authenticatedUser, newUserParam) => (
       UserService.create(authenticatedUser, { user: newUserParam })
     );
 
     describe('while server not setup', () => {
       it('returns error if first user is not admin', () => {
-        const createEditor = createUser(null, userParam({ role: 'editor' }));
-        const createSubscriber = createUser(null, userParam({ role: 'subscriber' }));
+        const createEditor = create(null, userParam({ role: 'editor' }));
+        const createSubscriber = create(null, userParam({ role: 'subscriber' }));
 
         return Promise.all([
           expect(createEditor).to.be.rejectedWith(UserService.SetupError),
@@ -29,7 +29,7 @@ describe('User service', () => {
       });
 
       it('adds admin user in setup mode', () => {
-        const createAdmin = createUser(null, userParam({ role: 'admin' }));
+        const createAdmin = create(null, userParam({ role: 'admin' }));
         const adminInDB = createAdmin.then(() => User.findOne());
 
         return expect(adminInDB).to.eventually.exist;
@@ -38,11 +38,11 @@ describe('User service', () => {
 
     describe('after server is setup', () => {
       // Add an admin account so server is no longer in setup mode.
-      beforeEach(() => createUser(null, userParam({ role: 'admin' })));
+      beforeEach(() => create(null, userParam({ role: 'admin' })));
 
       it('creating admin/editor user while unauthenticated returns error', () => {
-        const createAdmin = createUser(null, userParam({ role: 'admin' }));
-        const createEditor = createUser(null, userParam({ role: 'editor' }));
+        const createAdmin = create(null, userParam({ role: 'admin' }));
+        const createEditor = create(null, userParam({ role: 'editor' }));
 
         return Promise.all([
           expect(createAdmin).to.be.rejectedWith(UserService.PrivilegeError),
@@ -51,14 +51,14 @@ describe('User service', () => {
       });
 
       it('allows creation of subscriber accounts without an authenticated account', () => {
-        const createSubscriber = createUser(null, userParam({ role: 'subscriber' }));
+        const createSubscriber = create(null, userParam({ role: 'subscriber' }));
         return expect(createSubscriber).to.eventually.exist;
       });
 
       it('creates admin/editor user if authenticated as an admin user', () => {
         const credential = userParam({ role: 'admin' });
-        const createAdmin = createUser(credential, userParam({ role: 'admin' }));
-        const createEditor = createUser(credential, userParam({ role: 'editor' }));
+        const createAdmin = create(credential, userParam({ role: 'admin' }));
+        const createEditor = create(credential, userParam({ role: 'editor' }));
 
         return Promise.all([
           expect(createAdmin).to.eventually.exist,
@@ -80,6 +80,79 @@ describe('User service', () => {
       const nonexistantUser = UserService.findOne(null, { id: 'a'.repeat(24) });
 
       return expect(nonexistantUser).to.eventually.equal(null);
+    });
+  });
+
+  describe('find', () => {
+    // Swagger will insert default values for "page" and "per_page" so provide
+    // defaults here.
+    const find = (authenticatedUser, args) => (
+      UserService.find(authenticatedUser, {
+        page: 0,
+        per_page: 5,
+        ...args,
+      })
+    );
+    const adminUser = userParam({ role: 'admin' });
+    let expectedUsers;
+
+    // Insert 30 users into the database and store user JSON into "expectedUsers".
+    beforeEach(() => {
+      const createExpectedUsers = (() => {
+        const promises = [];
+        for (let i = 0; i < 30; i += 1) promises.push(User.create(userParam()));
+        return Promise.all(promises);
+      })();
+      const getUsersInDBOrder = () => User.find();
+      const convertUsersToJSON = users => users.map(u => u.toJSON());
+      const storeExpectedUsers = (users) => { expectedUsers = users; };
+      return createExpectedUsers
+        .then(getUsersInDBOrder)
+        .then(convertUsersToJSON)
+        .then(storeExpectedUsers);
+    });
+
+    it('returns error is user is not admin', () => {
+      const findAsSubscriber = find(userParam({ role: 'subscriber' }));
+      const findAsEditor = find(userParam({ role: 'editor' }));
+
+      return Promise.all([
+        expect(findAsSubscriber).to.be.rejectedWith(UserService.PrivilegeError),
+        expect(findAsEditor).to.be.rejectedWith(UserService.PrivilegeError),
+      ]);
+    });
+
+    it('default values return first page', () => {
+      const findWithDefaults = find(adminUser).then(
+        results => ({
+          totalUsers: results.totalUsers,
+          users: results.users.map(u => u.toJSON()),
+        }),
+      );
+      const firstFiveUsers = expectedUsers.slice(0, 5);
+
+      return expect(findWithDefaults).to.eventually.deep.equal({
+        totalUsers: 30,
+        users: firstFiveUsers,
+      });
+    });
+
+    it('returns requested page', () => {
+      const findPage2Results = find(adminUser, {
+        page: 1,
+        per_page: 20,
+      }).then(
+        results => ({
+          totalUsers: results.totalUsers,
+          users: results.users.map(u => u.toJSON()),
+        }),
+      );
+      const last10Users = expectedUsers.slice(20);
+
+      return expect(findPage2Results).to.eventually.deep.equal({
+        totalUsers: 30,
+        users: last10Users,
+      });
     });
   });
 });
