408aafc9ca0d48b39caf3a1998b8d100ed85dd16
strothj <jason@jasons.io>
2017-03-23 10:35:15 -0400

    Added Eslint and Mocha

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+/* eslint-disable padded-blocks */
 
+module.exports = {
+  // eslint-disable-next-line no-unused-vars
+  webpack: (config, { dev }) => {
+
+    // Ignore unit tests
+    config.module.rules.unshift({
+      test: /\.spec\.js$/,
+      use: ['ignore-loader'],
+    });
+
+    return config;
+  },
 };
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <style jsx>{`
+      div {
+        color: blue;
+      }
+    `}</style>
+    Hello world!
+  </div>
+);
